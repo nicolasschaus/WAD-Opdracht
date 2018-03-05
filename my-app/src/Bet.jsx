3e76d821693deb8b6443ddc3b3a52d7db16fbeab
nicolasschaus <nicolas.schaus@student.howest.be>
2018-03-05 19:57:07 +0100

    setState + propTypes added

--- my-app/src/Bet.jsx
+++ my-app/src/Bet.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { Component } from 'react';
 import BetItem from "./BetItem";
 
-const Bet = () => (  
+class Bet extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {scoreMatch1Thuis:0, scoreMatch1Uit:0, scoreMatch2Thuis:0, scoreMatch2Uit:0, scoreMatch3Thuis:0, scoreMatch3Uit:0}
+  }
+
+  handleChangeInput = (channel, value) => {
+    console.log("log vanuit app", value);
+    this.setState({[channel]: value});
+  }
+
+  
+  render() {
+  
+  const {scoreMatch1Thuis, scoreMatch1Uit, scoreMatch2Thuis, scoreMatch2Uit, scoreMatch3Thuis, scoreMatch3Uit} = this.state;
+  const {ploeg, styleElement} = this.props;
+
+  if (scoreMatch1Thuis > scoreMatch1Uit){
+    console.log("belgie wint");
+  } else if (scoreMatch1Thuis === scoreMatch1Uit) {
+    console.log("het is gelijk")
+  }  else {
+    console.log("panama wint");
+  }
+
+  return (
   <div>
     <h1>Voorspel de scores van de Rode Duivels op het WK!</h1>
-    <form>
+      <form>
         <ul>
-            <BetItem ploeg1="Belgie" ploeg2="Panama" />
-            <BetItem ploeg1="Belgie" ploeg2="Tunesië" />
-            <BetItem ploeg1="Engeland" ploeg2="Belgie" />
+          <li>
+            <BetItem onChange={value => this.handleChangeInput('scoreMatch1Thuis', value)} value={scoreMatch1Thuis} ploeg="Belgie" styleElement="left-style" />
+            <BetItem onChange={value => this.handleChangeInput('scoreMatch1Uit', value)} value={scoreMatch1Uit} ploeg="Panama" styleElement="right-style" /> 
+          </li>
+          <li>
+            <BetItem onChange={value => this.handleChangeInput('scoreMatch2Thuis', value)} value={scoreMatch2Thuis} ploeg="Belgie" styleElement="left-style" />
+            <BetItem onChange={value => this.handleChangeInput('scoreMatch2Uit', value)} value={scoreMatch2Uit} ploeg="Tunesie" styleElement="right-style" />
+          </li>
+          <li>
+            <BetItem onChange={value => this.handleChangeInput('scoreMatch3Thuis', value)} value={scoreMatch3Thuis} ploeg="Engeland" styleElement="left-style" />
+            <BetItem onChange={value => this.handleChangeInput('scoreMatch3Uit', value)} value={scoreMatch3Uit} ploeg="Belgie" styleElement="right-style" />
+          </li>
         </ul>
-        <button>Bevestig deze scores</button>
-    </form>
-  </div>
-);
+          <button>Bevestig deze scores</button>
+      </form>
+    </div>
+   );
+  };
+};
 
 export default Bet;
--- my-app/src/BetItem.jsx
+++ my-app/src/BetItem.jsx
@@ -1,31 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types"
 
-class BetItem extends Component {  // state gebruikt dus class ipv functie
-  constructor(props) { // constructor als 'klassieke' functie schrijven
-    super(props); //parent constructor oproepen, deze van Component dus
-    this.state = { score1: 0, score2: 0 };
+const BetItem = ({value, onChange, ploeg, styleElement}) => {
+
+  const handleChangeInput = e => {
+    const {value} = e.currentTarget;
+    onChange(parseInt(value,10));
+    console.log({value});
   }
 
-  handleClickItem = e => {
-     //enkel mogelijk om ordered op true te zetten
-    // this.setState({ordered: true})
-
-    // Om de state aan te passen op basis van de vorige state, kan je een functie meegeven ipv een object aan de setState()
-    // Deze functie krijgt dan de vorige state en de props mee als argumenten
-    console.log("hallo");
-    this.setState((prevState, props) => ({ ordered: !prevState.ordered }));
-  };
-
-  render = () => {
-    const { ploeg1, ploeg2 } = this.props;
-    const { score1, score2 } = this.state;
-
     return (
-      <li>
-        <span class={ploeg1}>{ploeg1}<input type="number" name={ploeg1} min="0" max="15"  value={score1} /></span>- <span class={ploeg2}><input type="number" name="{ploeg2}" min="0" max="15" value={score2} />{ploeg2}</span>
-      </li>
+      <label className={ploeg+" "+styleElement}>
+          <h1>{ploeg}</h1>
+          <input type="number" min="0" max="15" onChange={handleChangeInput} value={value} />
+          <span>&#45;</span>
+      </label>
     );
   };
+
+BetItem.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.number.isRequired,
+    ploeg: PropTypes.string.isRequired,
+    styleElement: PropTypes.string.isRequired
 }
 
 export default BetItem;
